6a30492102f26fc374dd2fda77dd0f54cf4370f8
lutfilaelina <lutfilaeli784@gmail.com>
2024-11-02 22:04:02 +0700

    Add files via upload

---
+++ Latihan18.java
@@ -0,0 +1,26 @@
+/*
+ * @author 
+ * NAMA     : Lutfi Laeli Nur Afiyah
+ * KELAS    : Teknik Informatika 
+ * NIM      : 23215046
+ * Deskripsi Program : Kelipatan
+ */ 
+
+package Pertemuan6;
+
+public class Latihan18 {
+    public static void main(String[] args) {
+        
+        for (double bilangan = 3.5; bilangan <= 35.0; bilangan += 3.5) {
+            System.out.println(bilangan);               
+    }}}
+
+/*Saya menggunakan perulangan for karena jumlah iterasi looping untuk mencetak  
+  kelipatan bilangan tersebut sudah diketahui, yaitu berhenti ketika iterasi 
+  mencapai angka 35.0 (10 perulangan)
+
+  Dalam looping for tersebut, program akan mencetak setiap kelipatan 3.5, 
+  dimulai dari 3.5 hingga mencapai 35.0 dengan menambahkan 3.5 pada variabel 
+  bilangan di setiap iterasi. 
+*/
+
